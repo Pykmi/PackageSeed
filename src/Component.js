186356a2ddad3903f3e29f0aaccb5b512411f36b
Tomi Kaistila <tomi@Raven.home>
2018-03-25 18:22:44 +0300

    Initial commit

---
+++ src/Component.js
@@ -0,0 +1,10 @@
+import React from 'react'
+//import PropTypes from 'prop-types'
+
+const TestComponent = () => {
+	return (
+		<div>Test component.../div>
+	)
+}
+
+export default TestComponent
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import TestComponent from './Component'
+
+export {
+	TestComponent
+}
